252b9589155d64749dbb1210038c6d5a4480ad72
Kunwar Aditya <adityakunwar0147@gmail.com>
2024-07-26 10:08:12 +0530

    final build push

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['https://draw-frontend.onrender.com'],
+    origin: ['https://draw-dot-app.vercel.app/'],
   },
 });
 
