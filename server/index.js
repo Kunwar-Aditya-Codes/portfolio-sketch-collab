81d5f4d0ce635d92fcd35ea17db0231af8dbbc05
Kunwar Aditya <adityakunwar0147@gmail.com>
2024-07-26 07:26:43 +0530

    port update

--- server/index.js
+++ server/index.js
@@ -36,6 +36,6 @@ io.on('connection', (socket) => {
   socket.on('clear', (boardId) => io.to(boardId).emit('clear'));
 });
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log('Server listening on port: 5000');
 });
